85698b2374de513936a75b0ddf238d141b75e49f
Данилина Анна Сергеевна <ri67s03@mwp11.csc.local>
2017-02-15 09:20:12 +0700

    Add new file

---
+++ calc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum, days;
+    printf("Введите сумму вклада:\n");
+    scanf("%d", &sum);
+    printf("Введите срок:\n");
+    scanf("%d", &days);
+    return 0;
+} 
