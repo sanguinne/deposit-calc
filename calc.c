fd326a99fbecf18e62bc80318a08830b1e099bf6
Данилина Анна Сергеевна <ri67s03@mwp11.csc.local>
2017-03-01 08:15:46 +0700

    Add calculating

--- calc.c
+++ calc.c
@@ -16,5 +16,42 @@ int main()
         printf("Срок вклада не может быть отрицательным или превышать 365 дней. Попробуйте еще раз.\n");
         scanf("%d", &days);
     }
-
+    if (sum <= 10000) {
+        if (days < 31) {
+            income = sum / 10;
+            result = sum - income;
+        }
+        else if ((days > 30) && (days < 121)) {
+            income = (sum / 100) * 2;
+            result = sum + income;
+        }
+        else if ((days > 120) && (days < 241)) {
+            income = (sum / 100) * 6;
+            result = sum + income;
+        }
+        else {
+            income = (sum / 100) * 12;
+            result = sum + income;
+        }
+    }
+    else {
+        if (days < 31) {
+            income = sum / 10;
+            result = sum - income;
+        }
+        else if ((days > 30) && (days < 121)) {
+            income = (sum / 100) * 3;
+            result = sum + income;
+        }
+        else if ((days > 120) && (days < 241)) {
+            income = (sum / 100) * 8;
+            result = sum + income;
+        }
+        else {
+            income = (sum / 100) * 15;
+            result = sum + income;
+        }
+    }
+    printf("Sum = %f,days =  %d, result = %f", sum, days, result);
+    return 0;
 }
