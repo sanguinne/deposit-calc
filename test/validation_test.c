1adcba14fcb6d46225848f9be1c18def55d98003
Данилина Анна Сергеевна <ri67s03@mwp11.csc.local>
2017-05-10 08:38:31 +0700

    Change the validation test

--- test/validation_test.c
+++ test/validation_test.c
@@ -11,3 +11,33 @@ CTEST(correct_data, data)
     const int expected = -1;
     ASSERT_EQUAL(expected, result);
 }
+CTEST(correct_data, data1)
+{
+    const double a = 20000;
+    const unsigned int b = 400;
+    
+    const int result = correct_data(a, b);
+    
+    const int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+CTEST(correct_data, data2)
+{
+    const double a = 1000;
+    const unsigned int b = 200;
+    
+    const int result = correct_data(a, b);
+    
+    const int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+CTEST(correct_data, data3)
+{
+    const double a = 10000;
+    const unsigned int b = 200;
+    
+    const int result = correct_data(a, b);
+    
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
