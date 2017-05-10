25925ebbf92555af845a20aba1a0b52aefc9a295
Данилина Анна Сергеевна <ri67s03@mwp11.csc.local>
2017-05-10 08:52:36 +0700

    Change the deposit test

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -11,3 +11,83 @@ CTEST(arithmetic_suite, calc)
     const double expected = 84800;
     ASSERT_DBL_NEAR(expected, result);
 }
+CTEST(arithmetic_suite, calc1)
+{
+    const double a = 80000;
+    const unsigned int b = 20;
+
+    const double result = calcularing(a, b);
+
+    const double expected = 72000;
+    ASSERT_DBL_NEAR(expected, result);
+}
+CTEST(arithmetic_suite, calc2)
+{
+    const double a = 80000;
+    const unsigned int b = 200;
+
+    const double result = calcularing(a, b);
+
+    const double expected = 84800;
+    ASSERT_DBL_NEAR(expected, result);
+}
+CTEST(arithmetic_suite, calc3)
+{
+    const double a = 80000;
+    const unsigned int b = 100;
+
+    const double result = calcularing(a, b);
+
+    const double expected = 81600;
+    ASSERT_DBL_NEAR(expected, result);
+}
+CTEST(arithmetic_suite, calc4)
+{
+    const double a = 80000;
+    const unsigned int b = 300;
+
+    const double result = calcularing(a, b);
+
+    const double expected = 89600;
+    ASSERT_DBL_NEAR(expected, result);
+}
+CTEST(arithmetic_suite, calc5)
+{
+    const double a = 800000;
+    const unsigned int b = 20;
+
+    const double result = calcularing(a, b);
+
+    const double expected = 720000;
+    ASSERT_DBL_NEAR(expected, result);
+}
+CTEST(arithmetic_suite, calc6)
+{
+    const double a = 800000;
+    const unsigned int b = 100;
+
+    const double result = calcularing(a, b);
+
+    const double expected = 824000;
+    ASSERT_DBL_NEAR(expected, result);
+}
+CTEST(arithmetic_suite, calc7)
+{
+    const double a = 800000;
+    const unsigned int b = 200;
+
+    const double result = calcularing(a, b);
+
+    const double expected = 864000;
+    ASSERT_DBL_NEAR(expected, result);
+}
+CTEST(arithmetic_suite, calc8)
+{
+    const double a = 800000;
+    const unsigned int b = 300;
+
+    const double result = calcularing(a, b);
+
+    const double expected = 920000;
+    ASSERT_DBL_NEAR(expected, result);
+}
